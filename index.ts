b71141fa60c56bd7da3a71c319f61cf45673d90b
Abraham Hernandez <abraham@abranhe.com>
2021-11-06 21:51:45 -0400

    Remove TODO from source code

--- index.ts
+++ index.ts
@@ -3,8 +3,6 @@ import { DependencyList, useEffect } from 'react';
 /**
  * `useEffect` Hook with support for await.
  *
- * TODO: Move to Typescript.
- *
  * @link https://stackoverflow.com/a/54637708/7602110
  */
 export default function useEffectAsync(
